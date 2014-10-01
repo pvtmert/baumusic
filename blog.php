d42850b39d4f80a59105df135551f046a3cfd365
S.P.A.R.K. Labs <root@spark>
2014-10-01 09:59:17 +0300

    created

---
+++ blog.php
@@ -0,0 +1,65 @@
+<?php
+
+?>
+
+<html>
+	<head>
+		<link rel=stylesheet type=text/css href=style.css />
+		<title> BAU Music Club's Blog </title>
+	</head>
+	<body>
+		<post>
+			<h2>paragraf basligi /// </h2>
+			<p>
+			paragraf ici yazi
+			<ul>
+				<li>madde 1</li>
+				<li>madde 2</li>
+			</ul>
+			</p>
+			<p>
+			baska bir paragraf
+			</p>
+		</post>
+		<hr>
+		<post>
+			duz bir blog girdisi (paragrafsiz)
+		</post>
+		<hr>
+		<post>
+			<pre>
+			asd
+			asd
+			asd
+			asd
+			asd
+			asd
+			asd
+			asda
+			sda
+			asd
+			asd
+			asd
+			asd
+			asd
+			asd
+			asd
+			asd
+			asd
+			asd
+			asd
+			asd
+			asd
+			asd
+			asd
+			asd
+			asd
+			asd
+			sad
+			sad
+			</pre>
+	</body>
+	<linkbar>
+	linkler
+	</linkbar>
+</html>
